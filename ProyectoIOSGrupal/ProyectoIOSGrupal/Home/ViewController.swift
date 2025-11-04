825c9b02f960765af28f599379f6cbfbe47ccb7c
Paula Escribano <noratiangb@gmail.com>
2025-11-04 12:10:11 +0100

    InfoPeliculas

--- ProyectoIOSGrupal/ProyectoIOSGrupal/Home/ViewController.swift
+++ ProyectoIOSGrupal/ProyectoIOSGrupal/Home/ViewController.swift
@@ -11,19 +11,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textoInicio: UILabel!
     @IBOutlet weak var botonDescubrirPelis: UIButton!
+    @IBOutlet weak var BotonFavoritos: UIButton!
     let VerFiltro1Segue = "showFiltro1"
-    
+    let VerFavoritosSegue = "VerFavoritos"
     //accion al pulsar el boton
     @IBAction func showFiltro1(_ sender: UIButton) {
         print("ver pantalla filtro1")
         self.performSegue(withIdentifier: VerFiltro1Segue, sender: self)
     }
     
+    @IBAction func VerFavoritos(_ sender: UIButton) {
+        print("ver pantalla favoritos")
+        self.performSegue(withIdentifier: VerFavoritosSegue, sender: self)
+    }
     //Configurar los textos del label y boton
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        BotonFavoritos.setTitle(String(localized: "VerFavoritos"), for: .normal)
         textoInicio.text = String(localized: "textoInicio")
         botonDescubrirPelis.setTitle(String(localized: "botonInicio"), for: .normal)
         botonDescubrirPelis.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
--- ProyectoIOSGrupal/ProyectoIOSGrupal/PeliculaPantallas/InfoPeliViewController.swift
+++ ProyectoIOSGrupal/ProyectoIOSGrupal/PeliculaPantallas/InfoPeliViewController.swift
@@ -15,15 +15,46 @@ class InfoPeliViewController: UIViewController {
     
     
     @IBOutlet weak var botonPruebaLista: UIButton!
-    @IBOutlet weak var anio: UILabel!
-    @IBOutlet weak var director: UILabel!
     @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var descrip: UILabel!
+    @IBOutlet weak var anio: UILabel!
+    @IBOutlet weak var genero: UILabel!
+    @IBOutlet weak var stars: UILabel!
+    
+    @IBOutlet weak var imagen: UIImageView!
+    
     
     var addBarBt: UIBarButtonItem?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         titulo.text = pelicula?.title
+               descrip.text = pelicula?.description
+        anio.text = pelicula != nil
+                ? "\(String(localized: "Anio")): \(pelicula!.year)"
+                : " "
+            
+            stars.text = pelicula != nil
+                ? "\(String(localized: "Puntuacion")): \(String(format: "%.1f", pelicula!.stars))"
+                : " "
+            
+            genero.text = pelicula != nil
+                ? "\(String(localized: "Genero")): \(pelicula!.genre)"
+                : " "
+               
+               if let imageUrlString = pelicula?.imageUrl,
+                  let url = URL(string: imageUrlString) {
+                   
+                   // Descargar la imagen en segundo plano
+                   URLSession.shared.dataTask(with: url) { data, response, error in
+                       if let data = data, let downloadedImage = UIImage(data: data) {
+                           // Actualizar la UI
+                           DispatchQueue.main.async {
+                               self.imagen.image = downloadedImage
+                           }
+                       }
+                   }.resume()
+               }
         
         self.addBarBt = UIBarButtonItem.init(title: String(localized: "anadirFav"), style: .plain, target: self, action: #selector(self.addBarBtAction))
         self.navigationItem.rightBarButtonItem = self.addBarBt
